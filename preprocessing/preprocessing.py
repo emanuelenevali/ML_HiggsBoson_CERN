52ee6da0be2ecc7cdb63cea2c0e45a912263e146
Emanuele Nevali <manu4375@gmail.com>
2022-10-15 19:36:48 +0200

    preproccessing update

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -27,24 +27,3 @@ def standardize(x):
     return x
 
 
-def data_cleaning(tx):
-    """
-    Deletion of features with more than 70% missing values and imposition of the median in the remaining features
-    """
-    N, D = tx.shape
-    missing_data = np.zeros(D)
-    cols_to_delete = []
-    for i in range(D):
-        missing_data[i] = np.count_nonzero(tx[:,i]==-999)/N
-
-        if missing_data[i]>0.7:
-            cols_to_delete.append(i)
-
-        elif missing_data[i]>0:
-            tx_feature = tx[:,i]
-            median = np.median(tx_feature[tx_feature != -999])
-            tx[:,i] = np.where(tx[:,i]==-999, median, tx[:,i])
-
-    tx[:,cols_to_delete]=0
-
-    return tx
