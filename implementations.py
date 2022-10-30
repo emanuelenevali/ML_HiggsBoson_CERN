ff4efbb853258eeff854461eb574cf87ab210ec7
Emanuele Nevali <manu4375@gmail.com>
2022-10-30 13:26:28 +0100

    bugs fixed

--- implementations.py
+++ implementations.py
@@ -167,15 +167,15 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     """
 
     w = initial_w
-
+    ##### reg_lr_compute_loss --> lr_calculate_loss
     ###################################################################################################################
     if max_iters == 0:
-        return w, reg_lr_compute_loss(y, tx, w, lambda_)
+        return w, lr_calculate_loss(y, tx, w, lambda_)
     ###################################################################################################################
 
     for _ in range(max_iters):
         g = reg_lr_compute_gradient(y, tx, w, lambda_)
         w = w - gamma * g
-        loss = reg_lr_compute_loss(y, tx, w, lambda_)
+        loss = lr_calculate_loss(y, tx, w, lambda_)
 
     return w, loss
